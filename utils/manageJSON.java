cb0d1bec0d77b03f89e915cc25ca550d86acdbae
Adrian Acarreta <alu.adrian.acarreta@mataro.epiaedu.cat>
2026-05-21 16:32:50 +0200

    feat(json: Add parser for every kind of data

--- utils/manageJSON.java
+++ utils/manageJSON.java
@@ -7,6 +7,12 @@ import org.json.simple.parser.JSONParser;
 public class manageJSON {
     Object obj = null;
     JSONArray articles = null;
+    manageDB db = null;
+
+    // Assignar la base de dades que farem servir
+    public manageJSON(manageDB db) {
+        this.db = db;
+    }
 
     // Carregar el fitxer JSON d'articles
     public void carregarJSON() {
@@ -70,6 +76,44 @@ public class manageJSON {
             }
         } catch (Exception e) {
             System.out.println("Primer has de carregar el JSON");
+        }
+    }
+
+    // Importar articles del JSON a la base de dades
+    public void importarArticlesBD() {
+        int afegits = 0;
+        int actualitzats = 0;
+        try {
+            comptarFamilies();
+
+            for (Object o:articles) {
+                JSONObject article = (JSONObject) o;
+
+                int id = getInt(article, "id");
+                String nom = (String) article.get("nom");
+                String familia = (String) article.get("familia");
+                int id_familia = db.consultaIdFamilia(familia);
+                Integer talla_coll = getInteger(article, "talla_coll");
+                Integer amplada_pit = getInteger(article, "amplada_pit");
+                Integer talla_cintura = getInteger(article, "talla_cintura");
+                Integer llargada_camal = getInteger(article, "llargada_camal");
+                double preu_base = getDouble(article, "preu_base");
+                int iva = getInt(article, "iva");
+                int stock = getInt(article, "stock");
+
+                if (db.existeixArticle(id)) {
+                    db.actualitzarArticle(id, nom, id_familia, talla_coll, amplada_pit, talla_cintura, llargada_camal, preu_base, iva, stock);
+                    actualitzats++;
+                } else {
+                    db.inserirArticle(id, nom, id_familia, talla_coll, amplada_pit, talla_cintura, llargada_camal, preu_base, iva, stock);
+                    afegits++;
+                }
+            }
+
+            System.out.println("(+) Articles afegits: " + afegits);
+            System.out.println("(+) Articles actualitzats: " + actualitzats);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
